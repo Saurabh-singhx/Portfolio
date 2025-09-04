1bb25c5ee07349edd74d3929883b805dc194cf39
Saurabh kumar <saurabh4442kumar@gmail.com>
2025-09-04 16:30:14 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import type React from "react"
 import type { Metadata } from "next"
-// import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { Montserrat, Open_Sans } from "next/font/google"
 import { Suspense } from "react"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "Saurabh's Portfolio",
 }
 
 const montserrat = Montserrat({
@@ -34,7 +31,6 @@ export default function RootLayout({
     <html lang="en" className={`${openSans.variable} ${montserrat.variable} dark antialiased`}>
       <body className="font-sans">
         <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
-        {/* <Analytics /> */}
       </body>
     </html>
   )
